9980728c14d3f79947f50075c0a82d71c2458974
Geek-ubaid <usmaniubaid@gmail.com>
2020-06-19 03:42:13 +0530

    downloading large files

--- forest_utils/tf_module.py
+++ forest_utils/tf_module.py
@@ -2,40 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import gdown
 
-import tensorflow as tf
+# import tensorflow as tf
 
-
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-
-
-class model_from_h5(tf.keras.models.Model):
-    def __init__(self, download_link, creds_file = 'credentials.json'):
-        super().__init__()
-        self.link = download_link
-        self.creds_file = creds_file
-        self.chunk_size = 32768
-        self.destination = "model.h5"
-        self.model = self.download()
-        
-    def download(self):
-        session = requests.session()
-        response = session.get(self.link, params = { 'id' : id }, stream = True)
-        
-        for key, value in response.cookies.items():
-            if key.startswith('download_warning'):
-                token = value
+class model_from_h5(object):
     
-        if token:
-            params = { 'id' : id, 'confirm' : token }
-            response = session.get(self.link, params = params, stream = True)
-        else:
-            print("No Token Found")
+    def __init__(self, url, output = 'model.h5'):
+        super().__init__()
+        
+        self.base_url = 'https://drive.google.com/uc?id='
+        self.url_id = self.get_complete_url(url)
+        self.output = output
+        
+    def get_complete_url(self, url):
+        split_url = url.split('/')
+        return self.base_url + split_url[5]
+        
+    def download_model(self):
+        try:
+            gdown.download(self.url_id, self.output, quiet=False)
+        except:
+            print("Some error occured")
             
-        with open(self.destination, "wb") as f:
-            for self.chunk_size in response.iter_content(self.chunk_size):
-                if self.chunk: # filter out keep-alive new chunks
-                    f.write(self.chunk)
-                    
-        return tf.keras.models.load_model(self.destination)
+    def load_model(self):
+        pass
+ 
     
