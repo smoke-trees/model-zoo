e1d7847744480d1b458b14faa7dcb8c35f948daa
aryamansriram <aryaman.sriram@gmail.com>
2020-08-16 23:46:20 +0530

    Added result.json and classes.py file for Flower_Net

---
+++ Flower_Net-CNN/classes.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class ensemble_Net(nn.Module):
+    pass
+class Flower_Net_1(nn.Module):
+    pass
+class Flower_Net_2(nn.Module):
+    pass
+class Flower_Net_3(nn.Module):
+    pass
